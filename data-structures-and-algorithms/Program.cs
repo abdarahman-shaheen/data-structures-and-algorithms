193210f67004c09e53c7dcb2d38f72513072169f
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-06-16 21:09:55 +0300

    change readme

--- data-structures-and-algorithms/Program.cs
+++ data-structures-and-algorithms/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            int[] array = new int[] {1,2,3,4,5};
+
+            int[] reverseArrat=Reverse(array);
+
+            for (int i = 0; i < reverseArrat.Length; i++)
+            {
+                Console.WriteLine(reverseArrat[i]);
+            }
+            */
         }
+        /*public static int[] Reverse(int[] array)
+        {
+            if (array.Length<0 || array==null)
+            {
+                throw new ArgumentNullException("array is null");   
+
+            }
+            else
+            {
+                int[] newArray = new int[array.Length];
+                int index = 0;
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    newArray[index] = array[i];
+                    index++;
+                }
+                return newArray;
+            }
+            
+        }*/
     }
+    
 }
